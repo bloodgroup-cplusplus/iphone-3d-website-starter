afc8eb2e9ede80c17286c3df775583e5fa372e6f
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-29 12:17:36 +0530

    added WebgiViewer

--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function DisplaySection() {
   return (
-    <div>
-      
+    <div className='display-section wrapper '>
+        <h2 className='title'>New</h2>
+        <p className='text'>Brilliant.</p>
+        <span className='description'> A display that's up to 2x brighter in the sun but not brighter than samsung's we tried though</span>
+        <button className='button'>Dont try me</button>
+        <button className='back-button'>GET TOP</button> 
+
     </div>
   )
 }
